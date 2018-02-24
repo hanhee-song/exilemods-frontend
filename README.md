32f13fe481448034d743c8474e290ee72aea2e5b
Hanhee Song <song.hanhee@gmail.com>
2018-02-23 21:46:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Custom React Skeleton
+
+To get started, run ```npm install```.
+To bundle, run ```npm run start```.
+To process the scss, run ```npm run sass```.
+To serve up index.html on localhost:8080 with auto-reload for changes in code, run ```npm run reload```.
+
+You may need to global install sass and reload via ```npm install -g sass reload```.
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/src",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }),
+    ]
+};
